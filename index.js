1758375ea5abb0754085ce51e93d777a388fef06
JuanChandoqui <193242@ids.upchiapas.edu.mx>
2022-06-09 12:19:52 -0500

    dockerfile added

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const router = express.Router();
 const app = express();
+
+const path = __dirname + '/views/';
 const port = 3000;
 const json = require('./resources/peliculas.json')
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello word!</h1>');
-});
+router.get('/', function(req, res){
+    res.sendFile(path + 'index.html');
+})
 
 app.get('/api/peliculas', (req, res) => {
     res.json(json)
 });
+
+app.use(express.static(path));
+app.use('/', router);
 
 app.use(cors());
 
